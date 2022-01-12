56181fbaae86865b1d3cd565eefc1528c37f9956
Kye <kyelindholm@gmail.com>
2022-01-12 11:48:13 -0500

    report review & mark review helpful now working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Pool, Client } = require('pg');
 
 express = require('express');
 const app = express();
+const PORT = 3000;
 
 
 const pool = new Pool({
@@ -20,14 +21,41 @@ const client = new Client({
 
 client.connect();
 
-const PORT = 3000;
+app.get('/reviews/', (req, res) => {
+  console.log('/reviews/');
+  res.status(200).send('/reviews/');
+});
 
-app.get('/test', (req, res) => {
-  pool.query('select * from reviews where id=5', (err, response) => {
-    console.log('recieved request!');
-    if (err) return console.log(err);
+app.get('/reviews/meta', (req, res) => {
+  console.log('/reviews/meta');
+  res.status(200).send('/reviews/meta');
+});
 
-    res.send(response.rows);
+app.post('/reviews', (req, res) => {
+  console.log('post /reviews');
+  res.status(200).send('post reviews');
+});
+
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  const review_id = req.params.review_id;
+  let helpfulness = 0;
+
+  client.query(`SELECT helpfulness FROM reviews WHERE id = ${review_id}`, (err, response) => {
+    helpfulness = response.rows[0].helpfulness;
+    let newHelpfulness = helpfulness + 1;
+    client.query(`UPDATE reviews SET helpfulness = ${newHelpfulness} WHERE id = ${review_id}`, (error) => {
+      if (error) throw error;
+      res.status(204).send();
+    });
+  })
+});
+
+app.put('/reviews/:review_id/report', (req, res) => {
+  const review_id = req.params.review_id;
+  client.query(`UPDATE reviews SET reported = true WHERE id = ${review_id}`, (err, response) => {
+    if (err) throw err;
+    console.log(`review #${review_id} reported`);
+    res.status(204).send();
   });
 });
 
